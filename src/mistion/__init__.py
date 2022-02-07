7036fd3762d64bc0939c8fc7cb1ec5ea71dadb40
lap1dem <vadym.bidula@gmail.com>
2022-02-07 09:51:05 -0500

    Import bug fixed (build_iri)

--- src/mistion/__init__.py
+++ src/mistion/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 from .IonModel import IonModel, srange, n_f, refr_angle, check_latlon
 
-from scripts.build_iri import build_iri
+from .scripts.build_iri import build_iri
 
 try:
     from .IonModelMPI import IonModelMPI
