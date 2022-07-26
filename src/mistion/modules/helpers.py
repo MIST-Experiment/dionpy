4fe55a6efa271bc36a5aefa1b50da132ac580c7f
lap1dem <vadym.bidula@gmail.com>
2022-07-25 22:43:53 -0400

    Basic documentation added to all functions.

--- src/mistion/modules/helpers.py
+++ src/mistion/modules/helpers.py
@@ -1,39 +1,39 @@
-import itertools
 import os
-from datetime import datetime
-from multiprocessing import Pool, cpu_count
-from typing import Tuple, Union
-import matplotlib.pyplot as plt
+from typing import Tuple, Union, Iterable, List
 
-import iricore
+import healpy as hp
 import numpy as np
-from scipy.interpolate import interp1d
+from ffmpeg_progress_yield import FfmpegProgress
+from pymap3d import aer2geodetic, Ellipsoid as pmEllipsoid
 from tqdm import tqdm
 
 from .ion_tools import srange
-from pymap3d import aer2geodetic
-import healpy as hp
 
 
 class TextColor:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+    """
+    Provides formatters for terminal text coloring.
+    """
+
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
 
 
-class Ellipsoid:
+class Ellipsoid(pmEllipsoid):
     """
     Custom ellipsoid for pymap3d package. Implements a simple sphere.
     """
 
     def __init__(self):
+        super().__init__()
         self.semimajor_axis = 6378100.0
         self.semiminor_axis = 6378100.0
         self.flattening = 0.0
@@ -41,68 +41,25 @@ class Ellipsoid:
         self.eccentricity = 0.0
 
 
-def none_or_array(vals):
+def none_or_array(vals: Union[None, Iterable]) -> Union[np.ndarray, None]:
+    """
+    Used for data loading from HDF files. Converts not None values to np.arrays.
+    """
     if vals is None:
         return None
     return np.array(vals)
 
 
-def polar_plot(
-        data: Tuple[np.ndarray, np.ndarray, np.ndarray],
-        dt: Union[datetime, None] = None,
-        pos: Union[Tuple[float, float, float], None] = None,
-        title=None,
-        barlabel=None,
-        plotlabel=None,
-        cblim=None,
-        saveto=None,
-        dpi=300,
-        cmap="viridis",
-        cbformat=None
-):
-    if plotlabel is None:
-        plotlabel = ""
-        if pos is not None:
-            plotlabel += f"Position: lat={pos[0]:.3f}, lon={pos[1]:.3f}\n"
-        if dt is not None:
-            plotlabel += "UTC time: " + datetime.strftime(dt, "%Y-%m-%d %H:%M")
-    cblim = cblim or (np.min(data[2]), np.max(data[2]))
-
-    fig = plt.figure(figsize=(8, 8))
-    ax: plt.Axes = fig.add_subplot(111, projection="polar")
-    img = ax.pcolormesh(
-        data[0],
-        data[1],
-        data[2],
-        cmap=cmap,
-        vmin=cblim[0],
-        vmax=cblim[1],
-        shading="auto",
-    )
-    ax.grid(color="gray", linestyle=":")
-    ax.set_theta_zero_location("S")
-    ax.set_rticks([90, 60, 30, 0], Fontsize=30)
-    ax.tick_params(axis='both', which='major', labelsize=11)
-    ax.tick_params(axis='y', which='major', labelcolor='gray')
-    # ax.scatter(0, 0, c="red", s=5)
-    plt.colorbar(img, fraction=0.042, pad=0.08, format=cbformat).set_label(label=barlabel, size=10)
-    plt.title(title, fontsize=14, pad=20)
-    plt.xlabel(plotlabel, fontsize=10)
-    if saveto is not None:
-        head, tail = os.path.split(saveto)
-        if not os.path.exists(head):
-            os.makedirs(head)
-        plt.savefig(saveto, dpi=dpi, bbox_inches='tight')
-        plt.close(fig)
-        return
-    return fig
-
-
-def polar_plot_star(pars):
-    return polar_plot(*pars)
+def is_iterable(x):
+    if isinstance(x, list) or isinstance(x, np.ndarray):
+        return True
+    return False
 
 
 def check_elaz_shape(el, az):
+    """
+    Checks shape and type of input elevation and azimuth.
+    """
     if not isinstance(el, float) and not isinstance(az, float):
         if isinstance(el, np.ndarray) and isinstance(el, np.ndarray):
             if not el.shape == az.shape:
@@ -113,34 +70,31 @@ def check_elaz_shape(el, az):
             )
 
 
-def sky2ll(el, az, height, pos):
+def sky2ll(
+    el: Union[float, np.ndarray],
+    az: Union[float, np.ndarray],
+    height: float,
+    pos: Tuple[float, float, float],
+) -> [Union[float, np.ndarray], Union[float, np.ndarray]]:
     """
-    Converts visible elevation and azimuth to geographic coordinates with given height of the visible point
+    Converts visible elevation and azimuth to geographic coordinates with given height of the visible point.
 
-    Parameters
-    ----------
-    el : float | np.ndarray
-        Elevation of observation(s) in deg.
-    az : float | np.ndarray
-        Azimuth of observation(s) in deg.
-    height : float
-        Height of observable point(s) in km.
-    pos: Tuple[float, float, float]
-        Geographical coordinates and height in m of the telescope
-
-    Returns
-    -------
-    obs_lat : float | np.ndarray
-        Observable geographical latitude.
-    obs_lon : float | np.ndarray
-        Observable geographical longitude.
+    :param el: Elevation of observation(s) in deg.
+    :param az: Azimuth of observation(s) in deg.
+    :param height: Height of observable point(s) in km.
+    :param pos: Geographical coordinates and height in m of the telescope
+    :return: Observable geographical latitude and longitude.
     """
     d_srange = srange(np.deg2rad(90 - el), height * 1e3)
     obs_lat, obs_lon, _ = aer2geodetic(az, el, d_srange, *pos, Ellipsoid())
     return obs_lat, obs_lon
 
 
-def elaz_mesh(gridsize):
+def elaz_mesh(gridsize: int) -> [np.ndarray, np.ndarray]:
+    """
+    :param gridsize: Grid resolution.
+    :return: Meshgrid of elevation and azimuth for all visible sky.
+    """
     el = np.linspace(0, 90, gridsize, endpoint=True)
     az = np.linspace(0, 360, gridsize)
     els, azs = np.meshgrid(el, az)
@@ -148,8 +102,33 @@ def elaz_mesh(gridsize):
 
 
 def eval_layer(
-        el, az, nside, position, hbot, htop, nlayers, obs_pixels, data, layer=None
+    el: Union[float, np.ndarray],
+    az: Union[float, np.ndarray],
+    nside: int,
+    position: Tuple[float, float, float],
+    hbot: float,
+    htop: float,
+    nlayers: int,
+    obs_pixels: List[int],
+    data: Union[float, np.ndarray],
+    layer: Union[int, None] = None,
 ):
+    """
+    Calculates interpolated values on healpix grid.
+
+    :param el: Elevation.
+    :param az: Azimuth.
+    :param nside: Resolution of healpix grid.
+    :param position:
+    :param hbot: Lower limit in [km] of the layer.
+    :param htop: Upper limit in [km] of the layer.
+    :param nlayers: Number of sub-layers used for intermediate calculations.
+    :param obs_pixels: List of pixel indices inside the visible disk on healpix sphere.
+    :param data: A data to interpolate.
+    :param layer: Number of sublayer from the precalculated sublayers.
+                  If None - an average over all layers is returned.
+    :return: Interpolated values at specified elevation and azimuth.
+    """
     check_elaz_shape(el, az)
     heights = np.linspace(hbot, htop, nlayers)
     map_ = np.zeros(hp.nside2npix(nside)) + hp.UNSEEN
@@ -173,57 +152,36 @@ def eval_layer(
         )
 
 
-def iri_star(pars):
-    return iricore.IRI(*pars)
-
-
-def interp_val(data1, data2, dt1, dt2, dt):
-    if dt1 == dt2:
-        return data1
-
-    x = np.asarray([0, (dt2 - dt1).total_seconds()])
-    y = np.asarray([data1, data2])
-    linmod = interp1d(x, y, axis=0)
-    x_in = (dt - dt1).total_seconds()
-    return linmod(x_in)
-
-
-def interp_val_star(pars):
-    return interp_val(*pars)
-
-
-def calc_interp_val(el, az, funcs, dts, *args, **kwargs):
-    data1 = funcs[0](el, az, *args, **kwargs)
-    data2 = funcs[1](el, az, *args, **kwargs)
-    return interp_val(data1, data2, *dts)
-
-
-def calc_interp_val_star(pars):
-    return calc_interp_val(*pars[:-2], *pars[-2], **pars[-1])
-
-
-def calc_interp_val_par(el, az, funcs, dts, pbar_desc, *args, **kwargs):
-    nproc = np.min([cpu_count(), len(funcs)])
-    disable = True if pbar_desc is None else False
-    rep_args = [args for i in range(len(dts))]
-    rep_kwargs = [kwargs for i in range(len(dts))]
-    with Pool(processes=nproc) as pool:
-        res = list(
-            tqdm(
-                pool.imap(
-                    calc_interp_val_star,
-                    zip(
-                        itertools.repeat(el),
-                        itertools.repeat(az),
-                        funcs,
-                        dts,
-                        rep_args,
-                        rep_kwargs,
-                    ),
-                ),
-                total=len(dts),
-                disable=disable,
-                desc=pbar_desc,
-            )
-        )
-    return np.array(res)
+def pic2vid(
+    imdir: str,
+    vidname: str,
+    savedir: str = "animations",
+    fps: int = 20,
+    desc: Union[str, None] = None,
+):
+    """
+    Renders existing set of pictures to mp4 video.
+    :param imdir: Location of images.
+    :param vidname: Name of the file to save.
+    :param savedir: Location of the file to save.
+    :param fps: Framerate - frames per second.
+    :param desc: Description of a progressbar. If None - the progressbar will not appear.
+    """
+    if not vidname.endswith(".mp4"):
+        vidname += ".mp4"
+    desc = desc or "Rendering video"
+    cmd = [
+        "ffmpeg",
+        "-r",
+        f"{fps}",
+        "-i",
+        os.path.join(imdir, "%06d.png"),
+        "-vcodec",
+        "libx265",
+        "-y",
+        os.path.join(savedir, vidname),
+    ]
+    ff = FfmpegProgress(cmd)
+    with tqdm(total=100, position=1, desc=desc) as pbar:
+        for progress in ff.run_command_with_progress():
+            pbar.update(progress - pbar.n)
